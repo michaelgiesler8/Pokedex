90e9b1cd2d3f194474864745243f79f3047188c4
Michael Giesler <michael.giesler1@icloud.com>
2024-11-27 15:56:53 -0700

    added core properties to the appstate

--- app/AppState.js
+++ app/AppState.js
@@ -1,11 +1,14 @@
 import { EventEmitter } from './utils/EventEmitter.js'
 import { createObservableProxy } from './utils/ObservableProxy.js'
 
-class ObservableAppState extends EventEmitter {
-
-  user = null
-  /**@type {import('./models/Account.js').Account | null} */
-  account = null
+class AppState extends EventEmitter {
+  account = null;
+  user = null;
+  regions = [];
+  wildPokemon = [];
+  myPokemon = [];
+  activeRegion = null;
+  activePokemon = null;
 }
 
 export const AppState = createObservableProxy(new ObservableAppState())
